e8cf7313864e64fbe0bd4da5a65a5b1712196a8d
Nadezhda Tsvetkova <nadezhda.tsvetkova@gmail.com>
2022-10-23 17:57:53 +0200

    changes in VehicleManagement class

--- src/def/VehicleManagement.java
+++ src/def/VehicleManagement.java
@@ -1,5 +1,7 @@
 package def;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleManagement {
@@ -49,7 +51,35 @@ public class VehicleManagement {
 		return trucks;
 	}
 	
-//	public double meanPrice() {
-//		
-//	}
+	public String meanPrice() {
+		double sumPrice = 0;
+		List<Vehicle> vehicles = this.getAll();
+		for(Vehicle v: vehicles) {
+			sumPrice += v.getPrice();
+		}
+		
+		DecimalFormat d = Vehicle.getDecimalFormat();
+		return d.format(sumPrice/vehicles.size());
+	}
+	
+	public List<Integer> getOldestVehicles(){
+		List<Integer> oldestIDs = new ArrayList<>();
+		
+		List<Vehicle> vehicles = this.getAll();
+		if(vehicles.isEmpty())
+			return new ArrayList<>();
+		
+		Vehicle oldest = vehicles.get(0);
+		for(Vehicle v: vehicles) {
+			if(v.getAge() > oldest.getAge())
+				oldest = v;
+		}
+		
+		for(Vehicle v: vehicles) {
+			if(oldest.getAge() == v.getAge())
+				oldestIDs.add(v.getId());
+		}
+		
+		return oldestIDs;
+	}
 }
